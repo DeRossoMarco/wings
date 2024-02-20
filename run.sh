9182f289ce6b7b11237804b876ff5e5fd70b0820
Marco De Rosso <marco.derosso@mail.polimi.it>
2024-02-20 15:15:43 +0100

    Added cluster script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin.bash
+#$ -cwd                 # workingDirectory
+#$ -j y
+#$ -N wings/wing
+#$ -S /bin/bash
+#$ -q all.q             # queueName
+#$ -pe mpi 32           # cpuNumber
+#$ -l h_rt=10:00:00
+
+module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
+module load openfoam
+
+# #!/bin/bash
+localDir='/global-scratch/USER/CASE'
