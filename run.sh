d3ade9eb86f69861ea0488b2eb90ebc1d1cb7136
Marco De Rosso <marco.derosso@mail.polimi.it>
2024-02-22 18:22:38 +0100

    Fix parallel running on cluster

--- run.sh
+++ run.sh
@@ -13,4 +13,57 @@ module load openfoam
 # #!/bin/bash
 localDir='/global-scratch/mderosso/wings'
 
-wing/Allrun
+decompDict="-decomposeParDict system/decomposeParDict.32"
+
+cp -r 0.orig 0
+
+touch wings.foam
+
+cp -rf system/simple/ system
+
+runApplication blockMesh
+
+runApplication surfaceFeatureExtract
+
+runApplication $decompDict decomposePar
+
+# Using distributedTriSurfaceMesh?
+if foamDictionary -entry geometry -value system/snappyHexMeshDict | \
+   grep -q distributedTriSurfaceMesh
+then
+    echo "surfaceRedistributePar does not need to be run anymore"
+    echo " - distributedTriSurfaceMesh will do on-the-fly redistribution"
+fi
+
+runParallel $decompDict snappyHexMesh -overwrite
+
+runParallel $decompDict extrudeMesh
+
+runParallel $decompDict transformPoints -translate "(0 0 -1)"
+
+runParallel $decompDict topoSet
+
+runParallel $decompDict checkMesh -writeFields '(nonOrthoAngle)' -constant
+
+#- For non-parallel running: - set the initial fields
+# restore0Dir
+
+#- For parallel running: set the initial fields
+restore0Dir -processor
+
+runParallel $decompDict potentialFoam -writephi -writePhi
+
+runParallel $decompDict $(getApplication)
+
+# runParallel $decomposeDict renumberMesh -overwrite
+
+cp -rf system/pimple/ system
+
+runParallel $decompDict $(getApplication)
+
+runApplication reconstructParMesh -constant
+
+runApplication reconstructPar
+
+#------------------------------------------------------------------------------
+
