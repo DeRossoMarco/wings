311582d1c372983e5a2d642b5bcb9340d23cad34
Marco De Rosso <marco.derosso@mail.polimi.it>
2024-03-12 10:04:39 +0100

    Run with 2 process

--- wing/cluster_run.sh
+++ wing/cluster_run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -N wings
+#$ -S /bin/bash
+#$ -q all.q             # queueName
+#$ -pe mpi 2           # cpuNumber
+#$ -l h_rt=10:00:00
+
+module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
+module load openfoam
+
+#!/bin/bash
 cd "${0%/*}" || exit
 
-decompDict="-decomposeParDict system/decomposeParDict.16"
+decompDict="-decomposeParDict system/decomposeParDict"
 
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
