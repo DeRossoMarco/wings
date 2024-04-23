45ea3427e4b2d814a98919bfc113dcd0c8754840
marco derosso <mderosso@master.local>
2024-04-23 11:35:51 +0200

    Fix cluster run with more than 16 processes

--- wing/cluster_run.sh
+++ wing/cluster_run.sh
@@ -2,65 +2,62 @@
 #$ -N single_run
 #$ -l h_rt=10:00:00
 
+CORES=$1
+
 module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
 module load openfoam
-
-#!/bin/bash
-localDir='/global-scratch/mderosso/wing'
-
-decompDict="-decomposeParDict system/decomposeParDict"
-
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
+#!/bin/bash
+localDir="/home/meccanica/mderosso/wings/wing"
+
 cp -r 0.orig 0
-
 touch wings.foam
-
 cp -rf system/simple/* system/
+mkdir log
 
-runApplication blockMesh
+echo "Running blockMesh"
+blockMesh > $localDir/log/log.blockMesh 2>&1
 
-runApplication surfaceFeatureExtract
+echo "Running surfaceFeatureExtract"
+surfaceFeatureExtract > $localDir/log/log.surfaceFeatureExtract 2>&1
 
-runApplication $decompDict decomposePar
+echo "Running decomposePar"
+decomposePar > $localDir/log/log.decomposePar 2>&1
 
-# Using distributedTriSurfaceMesh?
-if foamDictionary -entry geometry -value system/snappyHexMeshDict | \
-   grep -q distributedTriSurfaceMesh
-then
-    echo "surfaceRedistributePar does not need to be run anymore"
-    echo " - distributedTriSurfaceMesh will do on-the-fly redistribution"
-fi
+echo "Running snappyHexMesh in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES snappyHexMesh -parallel -overwrite < /dev/null > $localDir/log/log.snappyHexMesh 2>&1
 
-runParallel $decompDict snappyHexMesh -overwrite
+echo "Running extrudeMesh in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES extrudeMesh -parallel < /dev/null > $localDir/log/log.extrudeMesh 2>&1
 
-runParallel $decompDict extrudeMesh
+echo "Running transformPoints in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES transformPoints -parallel -translate "(0 0 -1)" < /dev/null > $localDir/log/log.transformPoints 2>&1
 
-runParallel $decompDict transformPoints -translate "(0 0 -1)"
+echo "Running topoSet in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES topoSet -parallel < /dev/null > $localDir/log/log.topoSet 2>&1
 
-runParallel $decompDict topoSet
+echo "Running checkMesh in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES checkMesh -parallel -writeFields '(nonOrthoAngle)' -constant < /dev/null > $localDir/log/log.checkMesh 2>&1
 
-runParallel $decompDict checkMesh -writeFields '(nonOrthoAngle)' -constant
-
-#- For non-parallel running: - set the initial fields
-# restore0Dir
-
-#- For parallel running: set the initial fields
 restore0Dir -processor
 
-runParallel $decompDict potentialFoam -writephi -writePhi
+echo "Running potentialFoam in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES potentialFoam -parallel -writephi < /dev/null > $localDir/log/log.potentialFoam 2>&1
 
-runParallel $decompDict $(getApplication)
+echo "Running simpleFoam in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES simpleFoam -parallel < /dev/null > $localDir/log/log.simpleFoam 2>&1
 
 # runParallel $decomposeDict renumberMesh -overwrite
 
 cp -rf system/pimple/* system/
 
-runParallel $decompDict $(getApplication)
+echo "Running pimpleFoam in parallel using $CORES processes"
+mpirun --hostfile machinefile.$JOB_ID -np $CORES pimpleFoam -parallel < /dev/null > $localDir/log/log.pimpleFoam 2>&1
 
-runApplication reconstructParMesh -constant
+echo "Running reconstructParMesh"
+reconstructParMesh -constant > $localDir/log/log.reconstructParMesh 2>&1
 
-runApplication reconstructPar
-
-#------------------------------------------------------------------------------
+echo "Running reconstructPar"
+reconstructPar > $localDir/log/log.reconstructPar 2>&1
 
