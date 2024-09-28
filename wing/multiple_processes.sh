7bc115c8af59569ce3a916ac0f08c436d4c00909
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2024-09-28 11:05:55 +0200

    Fix folder path

--- wing/multiple_processes.sh
+++ wing/multiple_processes.sh
@@ -8,9 +8,8 @@ rm -rf ../../multiple_cores && mkdir -p ../../multiple_cores
 
 for (( CORES=1; CORES<=$MAX_PROCESSES; CORES*=2 )) do
         echo "##### Running simulation with $CORES cores #####"
-        cp -f wing/system/decomposeParDict.orig wing/system/decomposeParDict
-        sed -i "s/CORES/$CORES/" wing/system/decomposeParDict
-        cd wing
+        cp -f system/decomposeParDict.orig system/decomposeParDict
+        sed -i "s/CORES/$CORES/" system/decomposeParDict
         ./cluster_run.sh
         # Copy results
         cp -rf postProcessing ../../multiple_cores/postProcessing_${CORES}
