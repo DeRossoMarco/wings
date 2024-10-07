8590a3be3299ddd3b011e423fba1bf0039dc7d73
Marco De Rosso <100926879+DeRossoMarco@users.noreply.github.com>
2024-10-07 10:20:04 +0200

    Delete plot.sh

--- plot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-file1="cdcl.txt"
-output="graph.png"
-gnuplot << EOFset terminal pngcairo enhanced font 'Arial,12'
-set output "$output"set xlabel "Valori sull'asse x"
-set ylabel "Valori sull'asse y"plot "$file1" using 1:2 with lines title "f(x)"
-EOF
